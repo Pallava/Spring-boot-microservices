e59c88258c550600195dd55a385a0c0e75937739
pallava <pallava@pallava-PC>
2021-08-14 13:39:51 +0530

    bbb

--- user-service/src/main/java/com/springboot/demo/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/springboot/demo/userservice/UserServiceApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 public class UserServiceApplication {
 
-	public static void main(String[] args ) {
+	public static void main(String[] args  ) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 	
